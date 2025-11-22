d2a6bf8dcfef5cd62475e8e6a90aab4479214008
Aurlynah <ojoly@et.esiea.fr>
2025-11-22 19:35:02 +0100

    Added MR support for project

---
+++ Assets/TimeVeil/Scripts/InputActionsManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Manage the current Input Actions
+/// </summary>
+public class InputActionsManager : MonoBehaviour
+{
+    /// <summary>
+    /// Instance of the Meta Player Input Actions
+    /// </summary>
+    static private MetaPlayerInputActions m_InstanceInputActions;
+
+    /// <summary>
+    /// Get Instance of the Meta Player Input Actions
+    /// </summary>
+    static public MetaPlayerInputActions InstanceInputActions
+    {
+        get { return m_InstanceInputActions; }
+    }
+
+    private void Awake()
+    {
+        // Instanciate Input Action
+        m_InstanceInputActions = new MetaPlayerInputActions();
+    }
+
+    void Start()
+    {
+        // Enable Input Action
+        m_InstanceInputActions.Enable();
+    }
+}
